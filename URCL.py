859fd8b75e35528678284e018db659ecd12221aa
Haku <60970967+Haku8866@users.noreply.github.com>
2020-10-02 16:04:51 +0100

    Complex - Core framework
    A system for using core instructions in place of complex ones has been set up. It is not fully developed yet, as complex instructions cannot define temporary variables.

--- URCL.py
+++ URCL.py
@@ -14,8 +14,8 @@ count = 0
 code = []
 var = []
 label_list = []
-labels = {
-    }
+labels = {}
+
 branching_ops = ["BRP","BRN","BNZ","BRZ","BNC","BRC","BRA","BRL","BRG","BRE","BNE","BEV","BOD","BGE","BLE"]
 operands = {"ADD":   3,"SUB":   3,"BSR":   3,"BSL":   3,"ADC":   3,"SBB":   3,"INC":   2,
             "DEC":   2,"MOV":   2,"IMM":   2,"XOR":   3,"AND":   3,"OR":    3,"NOR":   3,
@@ -26,9 +26,45 @@ operands = {"ADD":   3,"SUB":   3,"BSR":   3,"BSL":   3,"ADC":   3,"SBB":   3,"I
             "BOD":   2,"BEV":   2,"RSH":   2,"LSH":   2,"CMP":   2,"SRS":   3,"BSS":   3,
             "BLE":   3,"BGE":   3,"BITS":  2,"MINREG":1,"MINRAM":1,"IMPORT":0}
 
+cmplx_subs = {
+    "MLT": ["IMM <A> 0","BEV +2 <C>","ADD <A> <A> <B>","LSH <B> <B>","RSH <C> <C>","BNZ -4"],
+    "DIV": [],
+    "MOD": [],
+    "CAL": [],
+    "RET": [],
+    "PSH": [],
+    "POP": [],
+    "BRL": [],
+    "BRG": [],
+    "BRE": [],
+    "BNE": [],
+    "BOD": [],
+    "BEV": [],
+    "BLE": [],
+    "BGE": [],
+    "IN":  [],
+    "OUT": [],
+    "BSR": [],
+    "BSL": [],
+    "SRS": [],
+    "BSS": [],
+    "CMP": [],
+    "ADC": [],
+    "SBB": [],
+    }
+
+''' BSR CODE TO BE IMPLEMENTED AFTER TEMPORARY VARIABLES ARE SUPPORTED
+            //BSR $A $B $C
+MOV $X <C>  //set X to C
+MOV <A> <B> //copy B to A
+RSH <A> <A> //shift A right
+DEC $X      //decrement counter
+BNZ -2      //if not done, loop
+'''
+
 fname = input("File to compile? ")
 try:
-    f = open(fname)
+    f = open(fname, "r", encoding="utf8")
     file = []
     for line in f:
         file.append(line)
@@ -37,49 +73,96 @@ except:
     print(f"'{fname}' does not exist. (Is it in a different folder?)")
     input()
     sys.exit()
+
+file = list(filter(None, file))
 omitted = 0
 
 def snip(line):
     line = line.rstrip("\n")
     line = line.split("//")[0]
+    line = line.split(";")[0]
     line = line.strip()
-    line = line.replace(",","")
     line = line.split(" ")
     for x in range(0, len(line)):
         line[x] = line[x].strip()
+        striplist = []
+        for char in line[x]:
+            if char.isalpha() or char.isnumeric():
+                continue
+            elif char not in "#+-$: ":
+                striplist.append(char)
+        if striplist != []:
+            for char in striplist:
+                line[x] = line[x].replace(char,"")
     line = " ".join(line)
     return line
 
 for x in range(0, len(file)):
     file[x] = snip(file[x])
-    line = file[x].split(" ")
-    try:
-        ops = operands[line[0]]
-    except:
-        if line == "":
+file = list(filter(None, file))
+def fixLabels(file):
+    global count
+    for x in range(0, len(file)):
+        line = file[x].split(" ")
+        try:
+            ops = operands[line[0]]
+        except:
+            if line == "":
+                continue
+            if line[0][-1:] == ":":
+                label = line[0][:-1]
+                file[x+1] += f" ({label})"
+            else:
+                file[x] = ""
+        if line[0] in branching_ops:
+            label = line[1]
+            if label[0] == "+":
+                file[x+int(label[1:])] += f" (__LABEL__#{count})"
+            elif label[0] == "-":
+                file[x-int(label[1:])] += f" (__LABEL__#{count})"
+            else:
+                continue
+            file[x] = file[x].replace(label.strip(), f"__LABEL__#{count}")
+            label = f"__LABEL__#{count}"
+            label_list.append(label)
+            count += 1
+
+def replaceComplex(file):
+    for x in range(0, len(file)):
+        line = file[x].split()
+        if line[0] == "BITS" or line[0] == "IMPORT" or line[0] == "MINREG" or line[0] == "MINRAM":
             continue
-        if line[0][-1:] == ":":
-            label = line[0][:-1]
-            file[x+1] = snip(file[x+1]) + f" ({label})"
-        else:
-            file[x] = ""
-    if line[0] in branching_ops:
-        label = line[1]
-        if label[0] == "+":
-            file[x+int(label[1:])] = snip(file[x+int(label[1:])])
-            file[x+int(label[1:])] += f" (__LABEL__#{count})"
-        elif label[0] == "-":
-            file[x-int(label[1:])] = snip(file[x-int(label[1:])])
-            file[x-int(label[1:])] += f" (__LABEL__#{count})"
-        else:
-            continue
-        file[x] = file[x].replace(label.strip(), f"__LABEL__#{count}")
-        label = f"__LABEL__#{count}"
-        label_list.append(label)
-        count += 1
+        if ISA_Table[line[0]] == []:
+            if cmplx_subs[line[0]] != []:
+                repeat = True
+                newlines = cmplx_subs[line[0]]
+                ops = operands[line[0]]
+                try:
+                    label = " " + line[ops+1]
+                except:
+                    label = ""
+                for y in range(0, len(newlines)):
+                    newlines[y] = newlines[y].strip()
+                    if ops > 0:
+                        newlines[y] = newlines[y].replace("<A>", line[1])
+                    if ops > 1:
+                        newlines[y] = newlines[y].replace("<B>", line[2])
+                    if ops > 2:
+                        newlines[y] = newlines[y].replace("<C>", line[3])
+                    if y == 0 and label != "":
+                        newlines[y] += label
+                newlines.reverse()
+                file.pop(x)
+                for newline in newlines:
+                    file.insert(x, newline)
 
 file = list(filter(None, file))
-
+fixLabels(file)
+repeat = True
+while repeat:
+    repeat = False
+    replaceComplex(file)
+    fixLabels(file)
 print("\nURCL code:\n")
 for y in range(0, len(file)):
     label = ""
@@ -122,6 +205,8 @@ for y in range(0, len(file)):
     elif ISA_Table[line[0]] == []:
         omitted += 1
         continue
+    else:
+        newlines = ISA_Table[line[0]].copy()
     try:
         label = line[ops+1].strip()
         if label == "":
